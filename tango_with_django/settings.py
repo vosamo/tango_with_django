66f77a5ee148bfd2e4b069cf52cdc8bf2ced1556
vosamo <vosamo@outlook.com>
2016-07-11 11:50:06 +0800

    继承模板

--- tango_with_django/settings.py
+++ tango_with_django/settings.py
@@ -57,7 +57,7 @@ LOGIN_URL = '/rango/login/'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates'),],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'), ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -102,6 +102,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
