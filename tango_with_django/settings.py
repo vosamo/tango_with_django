0f2b779547eaf38541da80f0cfa7aad13c82c395
vosamo <vosamo@outlook.com>
2016-07-15 15:57:10 +0800

    使用django-registration-redux模块
    Signed-off-by: vosamo <vosamo@outlook.com>

--- tango_with_django/settings.py
+++ tango_with_django/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rango',
+    'registration',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -52,7 +53,12 @@ MIDDLEWARE_CLASSES = (
 )
 
 ROOT_URLCONF = 'tango_with_django.urls'
-LOGIN_URL = '/rango/login/'
+REGISTRATION_OPEN = True                # If True, users can register
+ACCOUNT_ACTIVATION_DAYS = 7     # One-week activation window; you may, of course, use a different value.
+REGISTRATION_AUTO_LOGIN = True  # If True, the user will be automatically logged in.
+LOGIN_REDIRECT_URL = '/rango/'  # The page you want users to arrive at after they successful log in
+LOGIN_URL = '/accounts/login/'  # The page users are directed to if they are not logged in,
+                                                                # and are trying to access pages requiring authentication
 
 TEMPLATES = [
     {
--- tango_with_django/urls.py
+++ tango_with_django/urls.py
@@ -1,5 +1,10 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from registration.backends.simple.views import RegistrationView
+
+class MyRegistrationView(RegistrationView):
+    def get_success_url(self, user=None):
+        return '/rango/'
 
 urlpatterns = [
     # Examples:
@@ -8,4 +13,7 @@ urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^rango/', include('rango.urls')),
+    #Add in this url pattern to override the default pattern in accounts.
+    url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
+    url(r'accounts/', include('registration.backends.simple.urls')),
 ]
